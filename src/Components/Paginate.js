ab410be8a5ff74feaa1027d0c188655e420a0538
Twinkle <tpardesh@localhost.localdomain>
2021-03-17 16:17:09 +0530

    Add switch for layout change option

--- src/Components/Paginate.js
+++ src/Components/Paginate.js
@@ -15,36 +15,36 @@ export default function Paginate(props) {
 
   //Set the page
   const onSetPage = (_event, pageNumber) => {
-    setPage(pageNumber)
+    setPage(pageNumber);
   };
 
   //Items to be displayed per page
   const onPerPageSelect = (_event, perPage) => {
-    setPerPage(perPage)
+    setPerPage(perPage);
   };
 
-  //Next set of Items 
+  //Next set of Items
   const onNextClick = (_event, page) => {
-    setPage(page)
-    setOffset((page - 1) * (perPage))
+    setPage(page);
+    setOffset((page - 1) * perPage);
   };
 
   //Previous set of Items
   const onPreviousClick = (_event, page) => {
-    setPage(page)
-    setOffset((page - 1) * (perPage));
+    setPage(page);
+    setOffset((page - 1) * perPage);
   };
 
   //First set of Items
   const onFirstClick = (_event, page) => {
-    setPage(page)
-    setOffset(0)
+    setPage(page);
+    setOffset(0);
   };
 
   //Last set of items
   const onLastClick = (_event, page) => {
-    setPage(page)
-    setOffset((page - 1) * (perPage));
+    setPage(page);
+    setOffset((page - 1) * perPage);
   };
 
   React.useEffect(() => {
@@ -57,16 +57,22 @@ export default function Paginate(props) {
   React.useEffect(() => {
     const SetImages = () => {
       if (props.screenshotsEN.length !== 0) {
-        const elementsLeft = props.screenshotsEN[0].images.slice(offset, (offset + perPage));
-        setElementLeft(elementsLeft)
+        const elementsLeft = props.screenshotsEN[0].images.slice(
+          offset,
+          offset + perPage
+        );
+        setElementLeft(elementsLeft);
       }
       if (props.screenshotsOther.length !== 0) {
-        const elementsRight = props.screenshotsOther[0].images.slice(offset, (offset + perPage));
-        setElementRight(elementsRight)
+        const elementsRight = props.screenshotsOther[0].images.slice(
+          offset,
+          offset + perPage
+        );
+        setElementRight(elementsRight);
       }
-    }
+    };
     SetImages();
-  }, [offset, perPage, screenshotsEN, screenshotsOther])
+  }, [offset, perPage, screenshotsEN, screenshotsOther]);
 
   const paginateEN = () => (
     <>
@@ -102,7 +108,7 @@ export default function Paginate(props) {
         onLastClick={onLastClick}
       />
     </>
-  )
+  );
 
   const paginateOther = () => (
     <>
@@ -119,34 +125,62 @@ export default function Paginate(props) {
         onFirstClick={onFirstClick}
         onLastClick={onLastClick}
       />
-      {/* For screenshots display side by side */}
-      <div id="image-compare">
-        <Split gutter="md">
-          <div>
-            {elementsLeft.length &&
-              <SplitItem>
-                <div>
-                  {elementsLeft.map((image, index) => (
-                    <img src={image} alt="" key={index} className="image" />
-                  ))}
-                </div>
-              </SplitItem>
-
-            }
-          </div>
-          <div>
-            {elementsRight.length &&
-              <SplitItem>
-                <div>
-                  {elementsRight.map((image, index) => (
-                    <img src={image} alt="" key={index} className="image" />
-                  ))}
-                </div>
-              </SplitItem>
-            }
-          </div>
-        </Split>
-      </div>
+      {props.changeLayout === true ? (
+        // {/* For screenshots display side by side */}
+        <div id="image-compare">
+          <Split gutter="md">
+            <div>
+              {elementsLeft.length && (
+                <SplitItem>
+                  <div>
+                    {elementsLeft.map((image, index) => (
+                      <img
+                        src={image}
+                        alt=""
+                        key={index}
+                        id={index}
+                        className="image_EN"
+                      />
+                    ))}
+                  </div>
+                </SplitItem>
+              )}
+            </div>
+            <div>
+              {elementsRight.length && (
+                <SplitItem>
+                  <div>
+                    {elementsRight.map((image, index) => (
+                      <img
+                        src={image}
+                        alt=""
+                        key={index}
+                        id={index}
+                        className="image_other"
+                      />
+                    ))}
+                  </div>
+                </SplitItem>
+              )}
+            </div>
+          </Split>
+        </div>
+      ) : (
+        // {/* //  For screenshots display one by one */}
+        <div>
+          {elementsLeft.map((image, index) => (
+            <div>
+              <img src={image} alt="" key={index} className="image1" />
+              <img
+                src={elementsRight[index]}
+                alt=""
+                key={index}
+                className="image1"
+              />
+            </div>
+          ))}
+        </div>
+      )}
       <Pagination
         widgetId="pagination-options-menu-bottom"
         itemCount={itemCount}
@@ -161,21 +195,23 @@ export default function Paginate(props) {
         onLastClick={onLastClick}
       />
     </>
-  )
+  );
 
   if (props.screenshotsEN.length === 0) {
     return <SimpleEmptyState />;
-  }
-  else if ((typeof itemCount !== "undefined") && (props.screenshotsOther.length === 0)) {
+  } else if (
+    typeof itemCount !== "undefined" &&
+    props.screenshotsOther.length === 0
+  ) {
     return <div className="mb-4">{paginateEN()}</div>;
-  }
-  else if ((typeof itemCount !== "undefined") && (props.screenshotsOther[0].id === props.screenshotsEN[0].id)) {
+  } else if (
+    typeof itemCount !== "undefined" &&
+    props.screenshotsOther[0].id === props.screenshotsEN[0].id
+  ) {
     return <div className="mb-4">{paginateEN()}</div>;
-  }
-  else {
+  } else {
     if (typeof itemCount !== "undefined")
-      return <div className="mb-4">{paginateOther()}</div>
-    else
-      return null
+      return <div className="mb-4">{paginateOther()}</div>;
+    else return null;
   }
 }
--- src/Screens/Versions.js
+++ src/Screens/Versions.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import BASE_URL from "../API/BASE_URL";
-import Paginate from "../Components/Paginate"
+import Paginate from "../Components/Paginate";
 import Breadcrumbs from "../Components/Breadcrumbs";
 import { useHistory } from "react-router";
 import constant from "../Constants/EnglishScreens.json";
@@ -25,6 +25,7 @@ export default function Versions(props) {
   const [screenshotsEN, setScreenshotsEN] = useState([]);
   const [itemCount, setItemCount] = useState();
   const [previousProductId] = useState(props.match.params.productid);
+  const [changeLayout, setChangeLayout] = useState();
   let history = useHistory();
 
   function handleDropdownChangeVersion(e) {
@@ -32,26 +33,33 @@ export default function Versions(props) {
   }
 
   function handleDropdownChangeLocale(e) {
-    setSelectLocales(e)
+    setSelectLocales(e);
   }
 
   //To get Versions and Locales of selected Product
   React.useEffect(() => {
     const fetchProductsVersionData = async () => {
-      const productsVersionData = await axios(`${BASE_URL}/products/${previousProductId}/product_versions`).catch(e => { console.error(e) });
+      const productsVersionData = await axios(
+        `${BASE_URL}/products/${previousProductId}/product_versions`
+      ).catch((e) => {
+        console.error(e);
+      });
 
-      const LocalesData = await axios(`${BASE_URL}/locales`).catch(e => { console.error(e) });
+      const LocalesData = await axios(`${BASE_URL}/locales`).catch((e) => {
+        console.error(e);
+      });
 
       //Return to Products if no version for selected Product is available
       if (productsVersionData.data.length !== 0) {
         setProductsVersion(productsVersionData.data);
-        setLocales(LocalesData.data)
-      }
-      else {
-        alert(" No Versions available for selected Product. Please select other Product");
+        setLocales(LocalesData.data);
+      } else {
+        alert(
+          " No Versions available for selected Product. Please select other Product"
+        );
         history.push({
-          pathname: "/"
-        })
+          pathname: "/",
+        });
       }
     };
     fetchProductsVersionData();
@@ -65,76 +73,92 @@ export default function Versions(props) {
       const screenshotsData = await axios(`${BASE_URL}/screenshots`, {
         params: {
           product_version_id: selectProductsVersion,
-          locale_id: selectLocales
+          locale_id: selectLocales,
         },
-      })
+      });
       const screenshotsENData = await axios(`${BASE_URL}/screenshots`, {
         params: {
           product_version_id: selectProductsVersion,
           locale_id: constant.englishLocaleId,
         },
-      })
+      });
       if (!screenshotsENData.data.length) {
-        alert("The selected Version have no English Screenshots")
-      }
-      else {
+        alert("The selected Version have no English Screenshots");
+      } else {
         setScreenshotsOther(screenshotsData.data);
         setScreenshotsEN(screenshotsENData.data);
         setItemCount(screenshotsENData.data[0].images.length);
       }
-    }
-    catch (err) {
+    } catch (err) {
       error = err;
     }
-  }
+  };
+
+  const onHandleLayout = async (name) => {
+    if (name === "Horizontal") {
+      setChangeLayout(true);
+    } else {
+      if (name === "Vertical") setChangeLayout(false);
+    }
+  };
+
   return (
     <>
       <PageSection variant={PageSectionVariants.light}>
         <Breadcrumbs />
-        {locales && productsVersion &&
+        {locales &&
+          productsVersion &&
           ((screenshotsOther && screenshotsOther.length !== 0) ||
-            (screenshotsEN && screenshotsEN.length !== 0)) &&
-          (
+            (screenshotsEN && screenshotsEN.length !== 0)) && (
             <PaginateForm
               selectProductsVersion={selectProductsVersion}
               selectLocales={selectLocales}
               productsVersion={productsVersion}
               locales={locales}
-              handleVersionChange={(e, event) => handleDropdownChangeVersion(e, event)}
-              handleLocaleChange={(e, event) => handleDropdownChangeLocale(e, event)}
+              screenshotsOther={screenshotsOther}
+              handleVersionChange={(e, event) =>
+                handleDropdownChangeVersion(e, event)
+              }
+              handleLocaleChange={(e, event) =>
+                handleDropdownChangeLocale(e, event)
+              }
               handleSubmit={onFormSubmit}
+              handleLayout={onHandleLayout}
             />
           )}
       </PageSection>
       <PageSection>
         {(screenshotsOther && screenshotsOther.length !== 0) ||
-          (screenshotsEN && screenshotsEN.length !== 0) ?
-          (
-            <Paginate
-              screenshotsOther={screenshotsOther}
-              screenshotsEN={screenshotsEN}
-              itemCount={itemCount}
-              elements={elements}
-              elementsRight={elementsRight}
-              elementsLeft={elementsLeft}
-              offset={offset}
-              currentPage={currentPage}
-              page={page}
-              perPage={perPage}
+        (screenshotsEN && screenshotsEN.length !== 0) ? (
+          <Paginate
+            screenshotsOther={screenshotsOther}
+            screenshotsEN={screenshotsEN}
+            itemCount={itemCount}
+            elements={elements}
+            elementsRight={elementsRight}
+            elementsLeft={elementsLeft}
+            offset={offset}
+            currentPage={currentPage}
+            page={page}
+            perPage={perPage}
+            changeLayout={changeLayout}
+          />
+        ) : (
+          locales &&
+          productsVersion && (
+            <SimpleForm
+              productsVersion={productsVersion}
+              locales={locales}
+              handleVersionChange={(e, event) =>
+                handleDropdownChangeVersion(e, event)
+              }
+              handleLocaleChange={(e, event) =>
+                handleDropdownChangeLocale(e, event)
+              }
+              handleSubmit={onFormSubmit}
             />
-          ) :
-          (
-            locales && productsVersion &&
-            (
-              <SimpleForm
-                productsVersion={productsVersion}
-                locales={locales}
-                handleVersionChange={(e, event) => handleDropdownChangeVersion(e, event)}
-                handleLocaleChange={(e, event) => handleDropdownChangeLocale(e, event)}
-                handleSubmit={onFormSubmit}
-              />
-            ))
-        }
+          )
+        )}
       </PageSection>
     </>
   );
