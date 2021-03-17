b2676e137a04f520c4bbf31c8f479426a36b7316
Twinkle <tpardesh@localhost.localdomain>
2021-03-17 16:17:09 +0530

    Assign IDs to each screenshot for image-comparison

--- src/Components/Paginate.js
+++ src/Components/Paginate.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Split, SplitItem } from "@patternfly/react-core";
-import { Pagination, PaginationVariant } from "@patternfly/react-core";
+import { Pagination, PaginationVariant, Badge } from "@patternfly/react-core";
 import SimpleEmptyState from "./SimpleEmptyState";
 
 export default function Paginate(props) {
@@ -12,39 +12,49 @@ export default function Paginate(props) {
   const [offset, setOffset] = useState();
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(10);
+  let [IdL, setIdL] = useState(0);
+  let [IdR, setIdR] = useState(0);
 
   //Set the page
   const onSetPage = (_event, pageNumber) => {
-    setPage(pageNumber)
+    setPage(pageNumber);
   };
 
   //Items to be displayed per page
   const onPerPageSelect = (_event, perPage) => {
-    setPerPage(perPage)
+    setPerPage(perPage);
   };
 
-  //Next set of Items 
+  //Next set of Items
   const onNextClick = (_event, page) => {
-    setPage(page)
-    setOffset((page - 1) * (perPage))
+    setIdL(IdL, IdL++);
+    setIdR(IdR, IdR++);
+    setPage(page);
+    setOffset((page - 1) * perPage);
   };
 
   //Previous set of Items
   const onPreviousClick = (_event, page) => {
-    setPage(page)
-    setOffset((page - 1) * (perPage));
+    setIdL((page - 1) * perPage);
+    setIdR((page - 1) * perPage);
+    setPage(page);
+    setOffset((page - 1) * perPage);
   };
 
   //First set of Items
   const onFirstClick = (_event, page) => {
-    setPage(page)
-    setOffset(0)
+    setIdL(0);
+    setIdR(0);
+    setPage(page);
+    setOffset(0);
   };
 
   //Last set of items
   const onLastClick = (_event, page) => {
-    setPage(page)
-    setOffset((page - 1) * (perPage));
+    setIdL((page - 1) * perPage);
+    setIdR((page - 1) * perPage);
+    setPage(page);
+    setOffset((page - 1) * perPage);
   };
 
   React.useEffect(() => {
@@ -57,16 +67,22 @@ export default function Paginate(props) {
   React.useEffect(() => {
     const SetImages = () => {
       if (props.screenshotsEN.length !== 0) {
-        const elementsLeft = props.screenshotsEN[0].images.slice(offset, (offset + perPage));
-        setElementLeft(elementsLeft)
+        const elementsLeft = props.screenshotsEN[0].images.slice(
+          offset,
+          offset + perPage
+        );
+        setElementLeft(elementsLeft);
       }
       if (props.screenshotsOther.length !== 0) {
-        const elementsRight = props.screenshotsOther[0].images.slice(offset, (offset + perPage));
-        setElementRight(elementsRight)
+        const elementsRight = props.screenshotsOther[0].images.slice(
+          offset,
+          offset + perPage
+        );
+        setElementRight(elementsRight);
       }
-    }
+    };
     SetImages();
-  }, [offset, perPage, screenshotsEN, screenshotsOther])
+  }, [offset, perPage, screenshotsEN, screenshotsOther]);
 
   const paginateEN = () => (
     <>
@@ -85,7 +101,12 @@ export default function Paginate(props) {
       />
       <div className="en_screens mb-4">
         {elementsLeft.map((image, index) => (
-          <img src={image} alt="" key={index} className="image" />
+          <div>
+            <div>
+              <Badge id={IdL++}>id: {IdL}</Badge>
+            </div>
+            <img src={image} alt="" key={index} className="image" />
+          </div>
         ))}
       </div>
       <Pagination
@@ -102,7 +123,7 @@ export default function Paginate(props) {
         onLastClick={onLastClick}
       />
     </>
-  )
+  );
 
   const paginateOther = () => (
     <>
@@ -123,27 +144,36 @@ export default function Paginate(props) {
       <div id="image-compare">
         <Split gutter="md">
           <div>
-            {elementsLeft.length &&
+            {elementsLeft.length && (
               <SplitItem>
                 <div>
                   {elementsLeft.map((image, index) => (
-                    <img src={image} alt="" key={index} className="image" />
+                    <div>
+                      <div>
+                        <Badge id={IdL++}>id: {IdL}</Badge>
+                      </div>
+                      <img src={image} alt="" key={index} className="image" />
+                    </div>
                   ))}
                 </div>
               </SplitItem>
-
-            }
+            )}
           </div>
           <div>
-            {elementsRight.length &&
+            {elementsRight.length && (
               <SplitItem>
                 <div>
                   {elementsRight.map((image, index) => (
-                    <img src={image} alt="" key={index} className="image" />
+                    <div>
+                      <div>
+                        <Badge id={IdR++}>id: {IdR}</Badge>
+                      </div>
+                      <img src={image} alt="" key={index} className="image" />
+                    </div>
                   ))}
                 </div>
               </SplitItem>
-            }
+            )}
           </div>
         </Split>
       </div>
@@ -161,21 +191,23 @@ export default function Paginate(props) {
         onLastClick={onLastClick}
       />
     </>
-  )
+  );
 
   if (props.screenshotsEN.length === 0) {
     return <SimpleEmptyState />;
-  }
-  else if ((typeof itemCount !== "undefined") && (props.screenshotsOther.length === 0)) {
+  } else if (
+    typeof itemCount !== "undefined" &&
+    props.screenshotsOther.length === 0
+  ) {
     return <div className="mb-4">{paginateEN()}</div>;
-  }
-  else if ((typeof itemCount !== "undefined") && (props.screenshotsOther[0].id === props.screenshotsEN[0].id)) {
+  } else if (
+    typeof itemCount !== "undefined" &&
+    props.screenshotsOther[0].id === props.screenshotsEN[0].id
+  ) {
     return <div className="mb-4">{paginateEN()}</div>;
-  }
-  else {
+  } else {
     if (typeof itemCount !== "undefined")
-      return <div className="mb-4">{paginateOther()}</div>
-    else
-      return null
+      return <div className="mb-4">{paginateOther()}</div>;
+    else return null;
   }
 }
